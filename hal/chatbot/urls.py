088f55521d3ae6d1c8bbce163c87b02ec5a0b31a
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2020-11-30 17:59:01 -0800

    Forms are working

--- hal/chatbot/urls.py
+++ hal/chatbot/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from chatbot import views
 
 urlpatterns = [
-    path('', views.home, name='chatbot')
+    path('', views.home, name='chatbot'),
+    path('<int:instance_id>/status', views.status_changed, name='on-off'),
+    path('<int:instance_id>/input', views.chat, name='input'),
 ]
--- hal/chatbot/views.py
+++ hal/chatbot/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'chatbot/HAL.html')
+
+
+def status_changed(request, instance_id):
+    print(request, instance_id)
+
+
+def chat(request, instance_id):
+    print(request, instance_id)
