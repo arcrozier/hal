e75794c1eb1c1c83ddcb960718a9e6b52bd977cb
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2020-12-05 19:54:49 -0800

    Configured settings for deployment

--- hal/settings.py
+++ hal/settings.py
@@ -20,7 +20,7 @@ with open(os.path.join(BASE_DIR, 'secret_key'), 'r') as f:
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['hal-chatbot.azurewebsites.net', "hal-chatbot.herokuapp.com"]
+ALLOWED_HOSTS = ['hal-chatbot.azurewebsites.net']
 
 
 # Application definition
