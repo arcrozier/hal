86eb8b1270ba7352c6aab35ad09885f3a5f5da7c
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2020-12-10 17:35:13 -0800

    Put project into deployment mode

--- hal/settings.py
+++ hal/settings.py
@@ -15,16 +15,12 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+with open(os.path.join(BASE_DIR, 'secret_key'), 'r') as f:
+    SECRET_KEY = f.read().strip()
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wl06v7@ebor(n50vhy)!jyks&!4y%a&i2h*4bt(p80hhee%+pz'
+DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hal-chatbot.azurewebsites.net', "hal-chatbot.herokuapp.com"]
 
 
 # Application definition
