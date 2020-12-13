8a227a793c0e35af769e98dc6e3fba60edd7f0a4
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2020-12-12 23:52:39 -0800

    Reordered apps

--- hal/settings.py
+++ hal/settings.py
@@ -30,14 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chatbot.apps.ChatbotConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatbot.apps.ChatbotConfig',
-    'channels',
 ]
 
 MIDDLEWARE = [
