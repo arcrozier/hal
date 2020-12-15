b9fb990bcc5fb4ddeddb2240f9e468070a064428
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2020-12-15 14:58:15 -0800

    Added explanation of who HAL is in splash screen

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from chatbot import views
 
 urlpatterns = [
-    path('', views.home, name='chatbot'),
+    path('', views.home, name='background'),
+    path('bot/', views.bot, name='chatbot'),
 ]
--- chatbot/views.py
+++ chatbot/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 
 
 def home(request):
+    return render(request, 'chatbot/background.html')
+
+
+def bot(request):
     return render(request, 'chatbot/HAL.html')
-
-
-def status_changed(request, instance_id):
-    print(request, instance_id)
-
-
-def chat(request, instance_id):
-    print(request, instance_id)
