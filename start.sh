e2d745e76a51199b77c3f8e1a39a12ff7b4b10c9
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2021-08-05 23:51:43 -0700

    Added logging

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
-hypercorn --bind localhost:8001 hal.asgi:application
+hypercorn --bind localhost:8001 hal.asgi:application >> hal.log
