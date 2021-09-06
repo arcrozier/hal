a6bebbcc1b2921ebee4e956c32be7ea9304da37c
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2021-09-06 12:34:32 -0700

    Updated start script to use venv

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P &>> hal.log
-{ /usr/local/bin/bin/hypercorn --bind localhost:8001 hal.asgi:application; } >> hal.log 2>&1
+cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
+{ venv/bin/hypercorn --bind localhost:8001 hal.asgi:application; } >> hal.log 2>&1
