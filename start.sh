4d33d55da0838d3cd4d1c48f43ecbb44e6ea62ec
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2025-01-26 22:32:37 -0800

    Updated logging

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
-{ .venv/bin/daphne --b localhost -p 8001 hal.asgi:application; } >> hal.log 2>&1
+.venv/bin/daphne --b localhost -p 8001 hal.asgi:application
